1751901d7840597c9dd569b662e97726200b9d8d
João Inácio <egnawake@gmail.com>
2022-05-12 15:21:53 +0100

    Test different collections

--- StringCollections/Program.cs
+++ StringCollections/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StringCollections
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> list = new List<string>();
+            list.Add("dog");
+            list.Add("parrot");
+            list.Add("pig");
+            list.Add("dog");
+            list.Add("bat");
+
+            PrintCollection(list, "List");
+
+
+            Stack<string> stack = new Stack<string>();
+            stack.Push("dog");
+            stack.Push("parrot");
+            stack.Push("pig");
+            stack.Push("dog");
+            stack.Push("bat");
+
+            PrintCollection(stack, "Stack");
+
+
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("dog");
+            queue.Enqueue("parrot");
+            queue.Enqueue("pig");
+            queue.Enqueue("dog");
+            queue.Enqueue("bat");
+
+            PrintCollection(queue, "Queue");
+
+
+            HashSet<string> set = new HashSet<string>();
+            set.Add("dog");
+            set.Add("parrot");
+            set.Add("pig");
+            set.Add("dog");
+            set.Add("bat");
+
+            PrintCollection(set, "HashSet");
+        }
+
+        private static void PrintCollection(IEnumerable<string> coll, string title)
+        {
+            Console.WriteLine(title);
+            foreach (string s in coll)
+            {
+                Console.WriteLine($"- {s}");
+            }
+            Console.WriteLine("");
         }
     }
 }
